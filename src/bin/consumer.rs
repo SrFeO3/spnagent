8eebe11c267025356d27c749946646629dd0e713
SrFeO3 <34501040+SrFeO3@users.noreply.github.com>
2026-03-23 14:02:07 +0900

    chore: minor cleanup

--- src/bin/consumer.rs
+++ src/bin/consumer.rs
@@ -21,17 +21,17 @@ use ep_lib::core;
 #[derive(Parser)]
 struct Args {
     #[arg(long, env = "SPN_HUB_HOSTNAME")]
-    fc_server_hostname: String,
+    spn_hub_hostname: String,
     #[arg(long, env = "SPN_HUB_PORT", default_value = "4433")]
-    fc_server_port: u16,
+    spn_hub_port: u16,
     #[arg(long, env = "SPN_AGENT_TRUST_CERTIFICATE_ROOT")]
-    fc_agent_trust_cert_ca: String,
+    spn_agent_trust_cert_ca: String,
     #[arg(long, env = "SPN_AGENT_CLIENT_CERTIFICATE")]
-    fc_agent_client_cert: String,
+    spn_agent_client_cert: String,
     #[arg(long, env = "SPN_AGENT_CLIENT_CERTIFICATE_KEY")]
-    fc_agent_client_cert_key: String,
+    spn_agent_client_cert_key: String,
     #[arg(long, env = "BIND_ADDRESS")]
-    fc_bind_address: String,
+    bind_address: String,
 }
 
 #[tokio::main]
@@ -39,12 +39,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     core::run_client_consumer(
-        &args.fc_server_hostname,
-        args.fc_server_port,
-        args.fc_agent_trust_cert_ca.as_ref(),
-        args.fc_agent_client_cert.as_ref(),
-        args.fc_agent_client_cert_key.as_ref(),
-        args.fc_bind_address.as_ref(),
+        &args.spn_hub_hostname,
+        args.spn_hub_port,
+        args.spn_agent_trust_cert_ca.as_ref(),
+        args.spn_agent_client_cert.as_ref(),
+        args.spn_agent_client_cert_key.as_ref(),
+        args.bind_address.as_ref(),
     )
     .await
 }
